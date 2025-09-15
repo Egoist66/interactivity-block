deb00901dcd805e40fa5dd7efe2654ced5ff48be
Farid Mahmudov <razormad666@gmail.com>
2025-09-15 09:00:19 +0300

    ready-plugin-build

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-blocks'), 'version' => '068183af989b17203a14');
---
+++ build/render.php
@@ -0,0 +1,2 @@
+
+<div class="crypto-price-container"></div>
